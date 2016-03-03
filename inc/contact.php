700e3ecc99dff6b975c2ebf41b4d4da4522c4909
Linda Castillor <linda@raidho.mx>
2016-03-02 19:00:49 -0600

    Se actualiza estructura extraíada de testbed para poder realizar cambios, la estructura del repo no coincidia o no estaba actualizada

--- inc/contact.php
+++ inc/contact.php
@@ -1,24 +1,24 @@
-<section>
-	<div class="contact<?php
-	if(is_single()) {
-		echo ' bg_grayM y_mtop';
-	} else {
-		echo ' bg_grayL';
-	} ?>">
-		<?php // If Image ?>
-		<div class="img" style=" background-image: url('img/590.jpg');">
-		</div>
-		<div class="form">
-			<h2><?php // if(contact title) else : ?>Queremos ser parte de tu proyecto</h2>
-			<p>Escríbenos y nos pondremos en contacto</p>
-			<?php // Select Form ?>
-			<form>
-				<input type="text" placeholder="Tu Nombre:">
-				<input type="text" placeholder="Email:">
-				<input type="text" placeholder="Teléfono:">
-				<textarea placeholder="Asunto:"></textarea>
-				<input type="submit" value="Enviar">
-			</form>
-		</div>
-	</div>
-</section>
+<?php
+
+if(get_field('contacto')):
+	while (have_rows('contacto')) :
+		the_row();
+
+		if(get_sub_field('on_off') == 'on') : ?>
+
+		<section>
+			<div class="contact bg_grayL y_mtop">
+				<div class="img" style=" background-image: url('<?php the_sub_field('img') ?>');">
+				</div>
+				<div class="form">
+					<h2><?php the_sub_field('title') ;?></h2>
+					<p>Escríbenos y nos pondremos en contacto</p>
+					<?php the_sub_field('forms'); ?>
+				</div>
+			</div>
+		</section><?php
+
+		endif;
+	endwhile;
+
+endif; ?>
